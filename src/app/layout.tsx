243fdf299968311fee15fdd742c6e1d2680cd48c
Gustavo Tavares <gt3oliveira@gmail.com>
2025-12-04 14:47:34 -0300

    feat: add header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "@/providers/auth";
+import { Header } from "@/components/header";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${poppins.className} antialiased`}
       >
         <NextAuthProvider>
+          <Header />
           {children}
         </NextAuthProvider>
       </body>
